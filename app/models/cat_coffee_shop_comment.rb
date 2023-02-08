a8e9890aeac2926671284afd27eb49764defca3b
gowasu-hydrangea <114853745+gowasu-hydrangea@users.noreply.github.com>
2023-02-08 14:48:35 +0000

    Fix Validates

--- app/models/cat_coffee_shop_comment.rb
+++ app/models/cat_coffee_shop_comment.rb
@@ -3,6 +3,6 @@ class CatCoffeeShopComment < ApplicationRecord
     belongs_to :customer
     belongs_to :cat_coffee_shop
     
-    validates :cat_coffee_shop_comment, presence: true
+    validates :cat_coffee_shop_comment, presence: true, length: {maximum:100}
     
 end
